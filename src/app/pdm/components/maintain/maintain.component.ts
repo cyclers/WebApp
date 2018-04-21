b4586215372abc729a38f6ceeefd6189c3f83681
Tarek <30959956+TarekMostafaHr@users.noreply.github.com>
2018-04-21 22:40:23 +0200

    test

--- src/app/pdm/components/maintain/maintain.component.ts
+++ src/app/pdm/components/maintain/maintain.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-maintain',
-  templateUrl: './maintain.component.html',
+  selector: "app-maintain",
+  templateUrl: "./maintain.component.html",
   styles: []
 })
 export class MaintainComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor() {}
+  infoPagesDropdown: Array<String> = [
+    "Contracts",
+    "Education",
+    "Schedules",
+    "Licenses",
+    "Immigration",
+    "Basic Pay",
+    "Allowances",
+    "Awards",
+    "Files"
+  ];
+  infoPages: Array<String> = [
+    "Overview",
+    "Actions",
+    "Org Assign.",
+    "Dates",
+    "Personal",
+    "Address",
+    "Family",
+    "History",
+    "Banks"
+  ];
+  ngOnInit() {}
 }
